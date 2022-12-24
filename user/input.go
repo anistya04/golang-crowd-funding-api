9a6efd38127469ac095f23d2385d3b64b9108269
Anistya Dwi Setiawan <anistyadwisetiawan04@gmail.com>
2022-12-24 22:10:53 +0700

    chore(user): bind struct user with email validation

--- user/input.go
+++ user/input.go
@@ -2,12 +2,12 @@ package user
 
 type RegisterInput struct {
 	Name       string `json:"name" binding:"required"`
-	Email      string `json:"email" binding:"required"`
+	Email      string `json:"email" binding:"required,email"`
 	Occupation string `json:"occupation" binding:"required"`
 	Password   string `json:"password" binding:"required"`
 }
 
 type LoginInput struct {
-	Email    string `json:"email" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
