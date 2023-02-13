ccf4ee8fa49b8173915b399ffacd4ddf9b8fbd9e
Anistya Dwi Setiawan <anistyadwisetiawan04@gmail.com>
2023-02-13 23:01:08 +0700

    refactor(campaign-service): change get campaigns

--- campaign/service.go
+++ campaign/service.go
@@ -1,8 +1,9 @@
 package campaign
 
+import "fmt"
+
 type Service interface {
-	GetAll() ([]Campaign, error)
-	GetByUserId(id int) ([]Campaign, error)
+	GetCampaigns(id int) ([]Campaign, error)
 }
 
 type service struct {
@@ -13,19 +14,19 @@ func NewService(r *repository) *service {
 	return &service{r}
 }
 
-func (s service) GetAll() ([]Campaign, error) {
-	campaigns, err := s.repository.FindAll()
+func (s service) GetCampaigns(UserId int) ([]Campaign, error) {
 
-	if err != nil {
-		return campaigns, err
+	if UserId != 0 {
+		fmt.Println("this is", UserId)
+		campaigns, err := s.repository.FindByUserId(UserId)
+		if err != nil {
+			return campaigns, err
+		}
+
+		return campaigns, nil
 	}
 
-	return campaigns, nil
-}
-
-func (s service) GetByUserId(id int) ([]Campaign, error) {
-	campaigns, err := s.repository.FindByUserId(id)
-
+	campaigns, err := s.repository.FindAll()
 	if err != nil {
 		return campaigns, err
 	}
